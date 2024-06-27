e83f4acc6d663e7a2558d29c0802fb0202ed5cb9
KayongoYongo <sknsammie10@gmail.com>
2024-06-27 18:38:09 +0300

    chatbot app

--- chatbot_project/chatbot_app/views.py
+++ chatbot_project/chatbot_app/views.py
@@ -5,6 +5,7 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 
+# can be modified for chatGPT
 # Load environmental variables from a .env file
 load_dotenv()
 
